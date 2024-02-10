5b09adffad43ead696df1146f7dccdcd99ce746d
LZY <1070402601@qq.com>
2024-02-10 14:48:09 +0800

    抄了点暮色源码为生成结构做铺垫

--- src/main/java/com/gaboj1/tcr/datagen/DataGenerators.java
+++ src/main/java/com/gaboj1/tcr/datagen/DataGenerators.java
@@ -28,6 +28,7 @@ public class DataGenerators {
         ModBlockTagGenerator blockTagGenerator = generator.addProvider(event.includeServer(),
                 new ModBlockTagGenerator(output, lookupProvider, helper));
         generator.addProvider(event.includeServer(), new ModItemTagGenerator(output, lookupProvider, blockTagGenerator.contentsGetter(), helper));
+        generator.addProvider(event.includeServer(), new ModBiomeTagGenerator(output, lookupProvider, helper));
         generator.addProvider(event.includeClient(), new ModLangGenerator(output));
         DatapackBuiltinEntriesProvider datapackProvider = new ModWorldGenProvider(output, lookupProvider);
         generator.addProvider(event.includeServer(), datapackProvider);
