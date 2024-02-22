2b72d07c7e2314c7b699056507bbf050a5411116
LZY <1070402601@qq.com>
2024-02-23 00:25:29 +0800

    调整攻击伤害，补全成就“近战法师”

--- src/main/java/com/gaboj1/tcr/item/custom/TreeSpiritWand.java
+++ src/main/java/com/gaboj1/tcr/item/custom/TreeSpiritWand.java
@@ -5,6 +5,8 @@ import com.gaboj1.tcr.TheCasketOfReveriesMod;
 import com.gaboj1.tcr.init.TCRModBlocks;
 import com.gaboj1.tcr.init.TCRModEntities;
 import com.gaboj1.tcr.util.ItemUtil;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.core.BlockPos;
@@ -15,7 +17,12 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.Item;
@@ -77,6 +84,33 @@ public class TreeSpiritWand extends MagicWeapon {
         }
 
         return InteractionResult.sidedSuccess(level.isClientSide);
+    }
+
+    //平A伤害调整
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot equipmentSlot) {
+        if (equipmentSlot == EquipmentSlot.MAINHAND) {
+            ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+            builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
+            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Item modifier", 7d, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Item modifier", -2.4, AttributeModifier.Operation.ADDITION));
+
+            return builder.build();
+        }
+        return super.getDefaultAttributeModifiers(equipmentSlot);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        if(pAttacker instanceof ServerPlayer serverPlayer) {
+            Advancement _adv = serverPlayer.getServer().getAdvancements().getAdvancement(new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "melee_mage"));
+            AdvancementProgress _ap = serverPlayer.getAdvancements().getOrStartProgress(_adv);
+            if (!_ap.isDone()) {
+                for (String criteria : _ap.getRemainingCriteria())
+                    serverPlayer.getAdvancements().award(_adv, criteria);
+            }
+        }
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
 
     @Override
