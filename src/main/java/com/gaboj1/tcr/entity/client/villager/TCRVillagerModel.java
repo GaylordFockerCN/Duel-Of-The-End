ef968439dc2ab02089380c2354110a223150070c
LZY <1070402601@qq.com>
2024-03-09 14:16:57 +0800

    修改村民模型构造器，使其可以转头

--- src/main/java/com/gaboj1/tcr/entity/client/villager/TCRVillagerModel.java
+++ src/main/java/com/gaboj1/tcr/entity/client/villager/TCRVillagerModel.java
@@ -3,24 +3,27 @@ package com.gaboj1.tcr.entity.client.villager;
 import com.gaboj1.tcr.TheCasketOfReveriesMod;
 import com.gaboj1.tcr.entity.custom.villager.TCRVillager;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.GeckoLib;
+import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 
 import java.util.Random;
 
-public class TCRVillagerModel extends GeoModel<TCRVillager> {
-//    String resName;
-//
-//    public TCRVillagerModel(String resName){
-//        this.resName = resName;
-//    }
+public class TCRVillagerModel extends DefaultedEntityGeoModel<TCRVillager> {
 
-    @Override
-    public ResourceLocation getModelResource(TCRVillager animatable) {
-//        System.out.println("model res"+animatable.getResourceName());
-        //应该是villager.geo.json的不过无所谓了
-        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "geo/pastoral_plain_villager.geo.json");
-//        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "geo/"+animatable.getResourceName()+".geo.json");
+    // We use the alternate super-constructor here to tell the model it should handle head-turning for us
+    //从GeckoLib的BatModel学的
+    public TCRVillagerModel(){
+        super(new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "pastoral_plain_villager"), true);
     }
+
+//    @Override
+//    public ResourceLocation getModelResource(TCRVillager animatable) {
+////        System.out.println("model res"+animatable.getResourceName());
+//        //应该是villager.geo.json的不过无所谓了
+//        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "geo/pastoral_plain_villager.geo.json");
+////        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "geo/"+animatable.getResourceName()+".geo.json");
+//    }
 
     @Override
     public ResourceLocation getTextureResource(TCRVillager animatable) {
@@ -32,10 +35,10 @@ public class TCRVillagerModel extends GeoModel<TCRVillager> {
         return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "textures/entity/villager/"+animatable.getResourceName()+".png");
     }
 
-    @Override
-    public ResourceLocation getAnimationResource(TCRVillager animatable) {
-//        System.out.println("ani res"+animatable.getResourceName());
-        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "animations/pastoral_plain_villager.animation.json");
-//        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "animations/"+animatable.getResourceName()+".animation.json");
-    }
+//    @Override
+//    public ResourceLocation getAnimationResource(TCRVillager animatable) {
+////        System.out.println("ani res"+animatable.getResourceName());
+//        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "animations/pastoral_plain_villager.animation.json");
+////        return new ResourceLocation(TheCasketOfReveriesMod.MOD_ID, "animations/"+animatable.getResourceName()+".animation.json");
+//    }
 }
--- src/main/java/com/gaboj1/tcr/entity/custom/villager/PastoralPlainVillagerElder.java
+++ src/main/java/com/gaboj1/tcr/entity/custom/villager/PastoralPlainVillagerElder.java
@@ -4,6 +4,7 @@ import com.gaboj1.tcr.entity.NpcDialogue;
 import com.gaboj1.tcr.entity.ai.goal.NpcDialogueGoal;
 import com.gaboj1.tcr.gui.screen.villager.PastoralPlainElderLinkListStreamScreen;
 import com.gaboj1.tcr.init.TCRModEntities;
+import com.gaboj1.tcr.init.TCRModItems;
 import com.gaboj1.tcr.network.TCRPacketHandler;
 import com.gaboj1.tcr.network.PacketRelay;
 import com.gaboj1.tcr.network.packet.NPCDialoguePacket;
@@ -43,12 +44,14 @@ public class PastoralPlainVillagerElder extends TCRVillager implements NpcDialog
     }
 
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new NpcDialogueGoal<>(this));//防乱跑
+        this.goalSelector.addGoal(1, new NpcDialogueGoal<>(this));//防乱跑但是好像没成功
         super.registerGoals();
     }
 
     @Override
     public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        this.setItemInHand(InteractionHand.MAIN_HAND, TCRModItems.ELDER_STAFF.get().getDefaultInstance());
+//        System.out.println(this.getItemInHand(InteractionHand.MAIN_HAND)+"client?"+this.isClientSide());
         if (hand == InteractionHand.MAIN_HAND) {
             if ( !this.level().isClientSide()) {
 //                if (DataManager.isWhite.getBool((ServerPlayer)player)) {
