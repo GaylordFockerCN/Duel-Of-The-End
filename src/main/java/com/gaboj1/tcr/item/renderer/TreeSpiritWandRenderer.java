2a8d8ed1cc374a85f8e9e9036d9ad63e7ce1526e
LZY <1070402601@qq.com>
2024-03-31 18:12:49 +0800

    添加发光层

--- src/main/java/com/gaboj1/tcr/item/renderer/TreeSpiritWandRenderer.java
+++ src/main/java/com/gaboj1/tcr/item/renderer/TreeSpiritWandRenderer.java
@@ -2,6 +2,11 @@ package com.gaboj1.tcr.item.renderer;
 
 import com.gaboj1.tcr.item.custom.boss_loot.TreeSpiritWand;
 import com.gaboj1.tcr.item.model.TreeSpiritWandModel;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import org.joml.Matrix4f;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
@@ -9,7 +14,16 @@ public class TreeSpiritWandRenderer extends GeoItemRenderer<TreeSpiritWand> {
 
     public TreeSpiritWandRenderer() {
         super(new TreeSpiritWandModel());
-//        addRenderLayer(new AutoGlowingGeoLayer<>(this));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 
+    //官方给的解法。。等更新新版后就不需要了
+    @Override
+    public void preRender(PoseStack poseStack, TreeSpiritWand animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        this.itemRenderTranslations = new Matrix4f(poseStack.last().pose());;
+        scaleModelForRender(this.scaleWidth, this.scaleHeight, poseStack, animatable, model, isReRender, partialTick, packedLight, packedOverlay);
+        if(!isReRender){
+            poseStack.translate(0.5f, 0.51f, 0.5f);
+        }
+    }
 }
