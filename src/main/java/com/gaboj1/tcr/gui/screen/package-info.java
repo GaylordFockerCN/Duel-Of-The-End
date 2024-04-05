1056e322ee9f339b60998821cea73814299c3520
LZY <1070402601@qq.com>
2024-04-05 19:15:57 +0800

    添加包说明

---
+++ src/main/java/com/gaboj1/tcr/gui/screen/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 从天境女武神源码中学习，并且用树以及递归构造按钮来进行优化，可以很方便地量产对话
+ * @author LZY
+ */
+package com.gaboj1.tcr.gui.screen;
---
+++ src/main/java/com/gaboj1/tcr/network/packet/server/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 从天境源码搬运过来延伸的，服务端发向客户端的数据。注意，在execute()中需要使用Minecraft.getInstance().player来获得客户端数据。
+ * @author LZY
+ */
+package com.gaboj1.tcr.network.packet.server;
