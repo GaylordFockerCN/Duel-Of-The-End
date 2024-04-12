8b32697ab205c4256697f0b8a6316ea09898638f
LZY <1070402601@qq.com>
2024-04-13 04:19:10 +0800

    添加原创声明

--- src/main/java/com/gaboj1/tcr/block/custom/BetterStructureBlock.java
+++ src/main/java/com/gaboj1/tcr/block/custom/BetterStructureBlock.java
@@ -2,10 +2,16 @@ package com.gaboj1.tcr.block.custom;
 
 import com.gaboj1.tcr.block.entity.BetterStructureBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.StructureBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BetterStructureBlock extends StructureBlock {
 
@@ -16,5 +22,11 @@ public class BetterStructureBlock extends StructureBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new BetterStructureBlockEntity(pPos, pState);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_49816_, @Nullable BlockGetter p_49817_, List<Component> components, TooltipFlag p_49819_) {
+        components.add(Component.literal("没错这玩意儿的独立mod的作者也是我"));
+        super.appendHoverText(p_49816_, p_49817_, components, p_49819_);
     }
 }
