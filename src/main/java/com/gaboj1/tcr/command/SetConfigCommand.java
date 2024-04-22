64abd92ac641448d0489591b711d138b353d41ef
P1nero <1070402601@qq.com>
2024-04-23 00:49:48 +0800

    增加设置完后信息显示。

--- src/main/java/com/gaboj1/tcr/command/SetConfigCommand.java
+++ src/main/java/com/gaboj1/tcr/command/SetConfigCommand.java
@@ -5,6 +5,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 
 public class SetConfigCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -12,15 +14,16 @@ public class SetConfigCommand {
                 .then(Commands.literal("set").requires((commandSourceStack) -> commandSourceStack.hasPermission(2))
                     .then(Commands.literal("enable_better_structure_block_load")
                         .then(Commands.argument("value", BoolArgumentType.bool())
-                                .executes((context) -> setData(BoolArgumentType.getBool(context, "value")))
+                                .executes((context) -> setData(BoolArgumentType.getBool(context, "value"), context.getSource().getPlayer()))
                         )
                     )
                 )
         );
     }
 
-    private static int setData(boolean value) {
+    private static int setData(boolean value, Player player) {
         TCRConfig.ENABLE_BETTER_STRUCTURE_BLOCK_LOAD.set(value);
+        player.displayClientMessage(Component.literal("enable_better_structure_block_load : "+value), false);
         return 0;
     }
 }
