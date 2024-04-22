a367d685774796a6bf12826b2df2caa63ca4226b
P1nero <1070402601@qq.com>
2024-04-23 00:51:42 +0800

    调整出生点坐标

--- src/main/java/com/gaboj1/tcr/worldgen/portal/TCRTeleporter.java
+++ src/main/java/com/gaboj1/tcr/worldgen/portal/TCRTeleporter.java
@@ -26,6 +26,15 @@ public class TCRTeleporter implements ITeleporter {
         return repositionEntity.apply(false);
     }
 
+    /**
+     * 在这里实现出生点。
+     * 不要相信IDE，pos==null判空是必须的！排查了很久！
+     * @param entity The entity teleporting before the teleport
+     * @param destinationLevel The world the entity is teleporting to
+     * @param defaultPortalInfo A reference to the vanilla method for getting portal info. You should implement your own logic instead of using this
+     *
+     * @return 更新后的传送信息。
+     */
     @Override
     public @Nullable PortalInfo getPortalInfo(Entity entity, ServerLevel destinationLevel, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
         PortalInfo pos;
@@ -35,8 +44,9 @@ public class TCRTeleporter implements ITeleporter {
         while (!destinationLevel.getBlockState(destinationPos).is(Blocks.AIR)){
             destinationPos = destinationPos.above();
         }
-        destinationPos = destinationPos.offset(18,0,0);//不然会诞生在房子里（
+        destinationPos = destinationPos.offset(20,0,0);//偏移一下不然会诞生在房子里（
         pos = new PortalInfo(destinationPos.getCenter(), Vec3.ZERO, entity.getYRot(), entity.getXRot());
+        //NOTE不要相信IDE，这里判空是必须的！
         return pos == null ? ITeleporter.super.getPortalInfo(entity, destinationLevel, defaultPortalInfo) : pos;
     }
 }
