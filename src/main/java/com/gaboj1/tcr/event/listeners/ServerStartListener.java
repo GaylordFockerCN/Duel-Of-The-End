aeeda86d8a63b9892b092d5db1a5d2ae05feeb8b
P1nero <1070402601@qq.com>
2024-04-25 17:34:12 +0800

    优化缓存目录识别，根据levelID来区分，更加准确，以免同名world用成重复缓存。

--- src/main/java/com/gaboj1/tcr/event/listeners/ServerStartListener.java
+++ src/main/java/com/gaboj1/tcr/event/listeners/ServerStartListener.java
@@ -5,16 +5,23 @@ import com.gaboj1.tcr.worldgen.biome.TCRBiomeProvider;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mod.EventBusSubscriber(modid = TheCasketOfReveriesMod.MOD_ID)
 public class ServerStartListener {
 
     /**
      * 获取存档名字，用于二次读取地图时用。
+     * 仅限服务器用，如果是单人玩则需要在选择窗口或者创建游戏窗口获取。因为LevelName是可重复的，LevelID才是唯一的...
+     * @see com.gaboj1.tcr.mixin.WorldListEntryMixin#injectedLoadWorld(CallbackInfo ci)
+     * @see com.gaboj1.tcr.mixin.CreateWorldScreenMixin#injected(CallbackInfoReturnable)
      */
     @SubscribeEvent
     public static void onServerAboutToStart(ServerAboutToStartEvent event){
-        TCRBiomeProvider.worldName = event.getServer().getWorldData().getLevelName();
+        if(TCRBiomeProvider.worldName.isEmpty()){
+            TCRBiomeProvider.worldName = event.getServer().getWorldData().getLevelName();
+        }
     }
 
 }
--- src/main/java/com/gaboj1/tcr/mixin/WorldListEntryMixin.java
+++ src/main/java/com/gaboj1/tcr/mixin/WorldListEntryMixin.java
@@ -1,6 +1,7 @@
 package com.gaboj1.tcr.mixin;
 
 import com.gaboj1.tcr.worldgen.biome.TCRBiomeProvider;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.worldselection.WorldSelectionList;
 import net.minecraft.world.level.storage.LevelSummary;
 import org.spongepowered.asm.mixin.Final;
@@ -11,15 +12,32 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * 用于删除地图缓存
+ * 用于删除地图缓存和获取存档名字。
  */
 @Mixin(WorldSelectionList.WorldListEntry.class)
 public class WorldListEntryMixin {
 
     @Shadow @Final private LevelSummary summary;
 
+    /**
+     * 通过监听删除世界来实现同步删除缓存文件。
+     * 有时候在想要不要把地图缓存一起放进世界文件夹，这样就省的删除了（
+     */
     @Inject(method = "doDeleteWorld()V", at = @At("RETURN"))
-    private void injected(CallbackInfo ci){
-        TCRBiomeProvider.deleteCache(this.summary.getLevelName());
+    private void injectedDoDeleteWorld(CallbackInfo ci){
+        TCRBiomeProvider.LOGGER.info("Deleting : " + summary.getLevelId() + ".dat -> "+ (TCRBiomeProvider.deleteCache(summary.getLevelId())?"SUCCESS":"FAILED"));
     }
+
+    @Inject(method = "loadWorld()V", at = @At("HEAD"))
+    private void injectedLoadWorld(CallbackInfo ci){
+        TCRBiomeProvider.worldName = summary.getLevelId();
+        TCRBiomeProvider.LOGGER.info("On loadWorld Sync : " + summary.getLevelId() + " >> TCRBiomeProvider.worldName");
+    }
+
+//    @Inject(method = "joinWorld()V", at = @At("HEAD"))
+//    private void injectedJoinWorld(CallbackInfo ci){
+//        TCRBiomeProvider.worldName = summary.getLevelId();
+//        TCRBiomeProvider.LOGGER.info("On joinWorld Sync : " + summary.getLevelId() + " >> TCRBiomeProvider.worldName");
+//    }
+
 }
